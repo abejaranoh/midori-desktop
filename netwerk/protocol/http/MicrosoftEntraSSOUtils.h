072ad01496c977bba02bd9d379ff131582978f5e
Ponchale <alfonso.hernandez@astian.org>
2024-10-26 09:18:18 -0500

    Bug 1768724 - Part 3: Add support for Microsoft SSO on macOS (Necko Part)  a=RyanVM

--- netwerk/protocol/http/MicrosoftEntraSSOUtils.h
+++ netwerk/protocol/http/MicrosoftEntraSSOUtils.h
@@ -3,11 +3,18 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 #ifndef MicrosoftEntraSSOUtils_h__
 #define MicrosoftEntraSSOUtils_h__
+
+
+#include "ErrorList.h"  // For nsresult
+#include "nsHttpChannel.h"
+
+
 namespace mozilla {
 namespace net {
 class nsHttpChannel;
 class MicrosoftEntraSSOUtils;  // Used in SSORequestDelegate
-void AddMicrosoftEntraSSO(nsHttpChannel* aChannel);
+nsresult AddMicrosoftEntraSSO(nsHttpChannel* aChannel,
+                              std::function<void()>&& aResultCallback);
 }  // namespace net
 }  // namespace mozilla
 #endif  // MicrosoftEntraSSOUtils_h__
